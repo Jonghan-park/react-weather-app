7906a126544bd41745efd8305de40b734d3e52df
Jonghan Park <jonghan.park@edu.sait.ca>
2022-09-01 00:01:44 -0600

    Update background images and css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import React from "react";
+import axios from "axios";
+
 function App() {
+  const api = {
+    key: "87e0929ac80376a13158f1a7657cd93d",
+    base: "https://api.openweathermap.org/data/2.5/",
+  };
+
   return (
-    <div className="App">
-      <h1>Hello</h1>
+    <div className="app">
+      <main>
+        <div className="search-box">
+          <input type="text" className="search-bar" placeholder="Search..." />
+        </div>
+      </main>
     </div>
   );
 }
